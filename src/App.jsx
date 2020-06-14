9a6363ec38282e93acb728e2cbfcd102b1a431e9
alexlo1 <alexdlo8967@gmail.com>
2020-06-14 14:51:05 -0700

    Clean up create-react-app skeleton

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import './App.css';
+
+export default function App() {
+  return (
+    <div className="app">
+      <h1>ACNH Fish and Bugs Catalog</h1>
+      <p>Coming soon!</p>
+    </div>
+  );
+}
